53fb2e512baeb89cfc0814d313fa2c8a7968f67f
konurbaev-e <forget>
2017-03-21 19:35:11 +0300

    refactoring

--- spring-token-auth/src/main/java/org/konurbaev/auth/config/WebSecurityConfig.java
+++ spring-token-auth/src/main/java/org/konurbaev/auth/config/WebSecurityConfig.java
@@ -11,8 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.konurbaev.auth.config.Constants.publicResources;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
